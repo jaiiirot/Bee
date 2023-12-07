d1893ad0df7d6bb9e9d1e43fb0936507ec3635e7
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-12-07 18:05:50 -0500

    se crea version mobile y se hacer ajustes de estilos en mobile y desktop del HOme

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 import "../../style/index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquareFacebook, faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faSquareFacebook,
+  faSquareInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
   return (
     <>
       <section className="hero-section">
         <div className="home-titulos">
-          <h2>TITULO</h2>
+          <h2>TITULO1</h2>
         </div>
+
+        <div className="home-titulo-mobile">
+          <h1>HACEMOS REALIDAD EL DISEÑO QUE SOÑASTE</h1>
+        </div>
+
         <div className="hero-img-container">
           <div className="hero-titulo">
             <h1>HACEMOS REALIDAD LO QUE SOÑASTE</h1>
           </div>
         </div>
-        <div className="logo-whatsappa-container">
-          <i>logo whatsapp en componente</i>
+
+        <div className="logo-whatsapp-container">
+          <FontAwesomeIcon className="icono-whatsapp" icon={faWhatsapp} />
         </div>
+
+        <div className="hero-img-container-mobile"></div>
       </section>
 
       <section className="personalizar-productos-section">
         <div className="home-titulos">
-          <h2>TITULO</h2>
+          <h2>PRODUCTOS PERSONALIZADOS</h2>
         </div>
         <div className="personalizar-productos-container">
           <div className="indumentaria-escolar personalizar-productos-container-img">
@@ -45,7 +57,50 @@ const Home = () => {
       </section>
 
       <section className="formulario-section">
-        <p>Aqui va el componente de formulario</p>
+        <div className="home-titulos">
+          <h2>ENVIANOS TU CONSULTA</h2>
+        </div>
+
+        <form action="" method="post">
+          <label htmlFor="nombre"></label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            placeholder="NOMBRE"
+            required
+          ></input>
+
+          <label htmlFor="correo"></label>
+          <input
+            type="email"
+            id="correo"
+            name="correo"
+            placeholder="Correo Electrónico"
+            required
+          ></input>
+
+          <label htmlFor="numero"></label>
+          <input
+            type="tel"
+            id="numero"
+            name="numero"
+            placeholder="Número de Teléfono"
+            required
+          ></input>
+
+          <label htmlFor="mensaje"></label>
+          <textarea
+            id="mensaje"
+            name="mensaje"
+            placeholder="Mensaje"
+            rows="4"
+            required
+          ></textarea>
+          <div className="formulario-boton-container">
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
       </section>
       <section className="redes-section">
         <div className="home-titulos">
@@ -61,11 +116,17 @@ const Home = () => {
         </div>
       </section>
       <section className="info-tienda-container">
-        <div className="iconos-tienda-container">
-          <i>Direccion</i>
-          <i>Horario</i>
-          <i>Telefono</i>
-        </div>
+        <ul className="iconos-tienda-container">
+          <li className="icono-hexagono">
+            <i>Direccion</i>
+          </li>
+          <li className="icono-hexagono">
+            <i>Horario</i>
+          </li>
+          <li className="icono-hexagono">
+            <i>Telefono</i>
+          </li>
+        </ul>
       </section>
     </>
   );
