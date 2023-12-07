ef7ff3f6fe85c2aeab225675a17442b8558971ab
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-12-07 09:31:43 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes,Route } from 'react-router'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contacto from "./components/page/Contacto";
+import Nosotros from "./components/page/Nosotros";
+import "./style/main.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Contacto />} />
+        <Route path="/about" element={<Nosotros />} />
       </Routes>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/image/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import imgLogo from "../../assets/icon.png";
+
+function Logo() {
+  return (
+    <figure>
+      <img src={imgLogo} alt="Logo de Bee" />
+    </figure>
+  );
+}
+
+export default Logo;
