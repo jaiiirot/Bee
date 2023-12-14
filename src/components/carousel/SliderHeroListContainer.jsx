2035676bfb5259a32567d02a241a0b7dc161909a
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-12-14 14:32:29 -0300

    haciendo productos

--- src/components/carousel/SliderHeroListContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import SliderHeroList from "./SliderHeroList";
-
-function SliderHeroListContainer() {
-  return <SliderHeroList />;
-}
-
-export default SliderHeroListContainer;
--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import IconoInstagram from "../../assets/icon/icono-instagram.svg";
 import IconoFacebook from "../../assets/icon/icono-facebook.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SliderHeroListContainer from "../carousel/SliderHeroListContainer";
-import { NavLink, Link } from "react-router-dom";
+// import SliderHeroListContainer from "../carousel/SliderHeroListContainer";
+// import { NavLink, Link } from "react-router-dom";
 import Formulario from "../formulario/Formulario";
 
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
 const Home = () => {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      SUBLIMACIÓN Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+      sequi aperiam aliquid tenetur maiores sit cum, commodi error sint rerum?
+      Consequatur cumque molestias quas, architecto omnis modi maiores suscipit
+      delectus! Quam facere quibusdam dolorum doloremque? Iure, nisi eveniet
+      maiores minima atque velit quas sed itaque nobis odio esse debitis quis
+      perferendis mollitia rerum beatae nostrum reiciendis explicabo neque
+      aliquam. Rerum. Autem, facere? Eaque reiciendis nobis, beatae aperiam
+      consectetur officiis exercitationem quas perferendis voluptas veniam
+      nostrum distinctio facere quod! Quaerat praesentium consequatur eligendi
+      tempora officiis tenetur possimus culpa est molestias pariatur. Veniam
+      odio aliquam ullam nemo sequi sit fugit molestiae voluptatem, cumque
+      laudantium iure minima nam voluptatibus laborum delectus voluptates. At
+      doloremque consequatur quas magni nam commodi provident illum tempore
+      nemo? Eos necessitatibus quos obcaecati? Ea hic nulla inventore incidunt
+      dicta deleniti atque animi asperiores eius distinctio soluta magni
+      praesentium, officiis similique omnis. Eius deserunt quidem vero iure
+      omnis ipsa eligendi.A
+    </Tooltip>
+  );
+
   return (
     <>
       <section className="hero-section">
@@ -23,7 +48,7 @@ const Home = () => {
 
       <section className="hero-mobile">
         <div className="hero-carusel-container-mobile">
-          <SliderHeroListContainer />
+          {/* <SliderHeroListContainer /> */}
         </div>
 
         <div className="formulario-boton-container-mobile">
@@ -33,9 +58,16 @@ const Home = () => {
 
       <section className="nuestros-estampados-section">
         <div className="info-tienda-hexagonosFila1">
-          <div className="hexagon">
-            <p>SUBLIMACIÓN</p>
-          </div>
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <div className="hexagon">
+              <p>SUBLIMACIÓN</p>
+            </div>
+          </OverlayTrigger>
+
           <div className="hexagon">
             <p>TERMOVINILO</p>
           </div>
