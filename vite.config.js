3c9d5978ae6bab8e6c6c83407da9fee8231d0a90
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-01-15 14:57:40 -0300

    new deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Bee",
   plugins: [react()],
-  base: "https://jaiiirot.github.io/Bee/",
 });
