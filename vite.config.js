bf94faee459d04fbc56c213daefc4007e2f9c4a9
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-02-28 14:12:28 -0300

    add domain personality

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bee/#/",
+  base: "http://beemendoza.com/#/",
   plugins: [react()],
 });
